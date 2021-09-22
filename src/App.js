f98ed06a5d17909577b7d7876f5c89a044f9a654
Robby Noyes <robertjnoyes1@gmail.com>
2021-09-22 10:42:38 -0700

    created template layout for forms, sections for each input area

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+  });
+  const [newName, setNewName] = useState('');
+
+  const addPersonalInfo = (event) => {
+    event.preventDefault();
+  };
+
+  const handleName = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="header">
+        <div>
+          <h1>CV Builder</h1>
+        </div>
+      </div>
+      <div className="contentContainer">
+        <section className="general">
+          <h2>General Info</h2>
+          <form>
+            <input placeholder="Full Name" />
+            <input placeholder="Email" />
+            <input placeholder="Phone Number" type="tel" />
+            <input placeholder="Address" />
+          </form>
+        </section>
+        <section className="education">
+          <h2>Education and Training</h2>
+          <form className="educationForm">
+            <input placeholder="University Name" />
+            <input placeholder="City" />
+            <input placeholder="Degree" />
+            <input placeholder="Area Of Study" />
+            <input placeholder="From" />
+            <input placeholder="To" />
+          </form>
+          <button>Delete</button>
+          <button>Add</button>
+        </section>
+        <section className="experience">
+          <h2>Experience</h2>
+          <form className="experienceForm">
+            <input placeholder="Position" />
+            <input placeholder="Company" />
+            <input placeholder="City" />
+            <input placeholder="From" />
+            <input placeholder="To" />
+          </form>
+          <button>Delete</button>
+          <button>Add</button>
+        </section>
+        <button type="submit">Submit</button>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
