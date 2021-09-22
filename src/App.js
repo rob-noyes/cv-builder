22f2e89a35775cdd484176191c020d0406c81415
Robby Noyes <robertjnoyes1@gmail.com>
2021-09-22 14:55:43 -0700

    General form completed, connected useState to component

--- src/App.js
+++ src/App.js
@@ -2,34 +2,75 @@ import './App.css';
 import React, { useState } from 'react';
 import Header from './components/Header';
 import General from './components/General';
-import Education from './components/Education';
-import Experience from './components/Experience';
+// import Education from './components/Education';
+// import Experience from './components/Experience';
 
 const App = () => {
   const [personalInfo, setPersonalInfo] = useState({
-    fullName: '',
-    email: '',
-    phoneNumber: '',
-    address: '',
+    fullName: 'Robby',
+    email: 'robby7630@gmail.com',
+    phoneNumber: '513-314-1687',
+    address: '6928 N Greenwich Ave',
   });
   const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [newAddress, setNewAddress] = useState('');
 
   const addPersonalInfo = (event) => {
     event.preventDefault();
+    const newPersonalInfo = {
+      fullName: newName,
+      email: newEmail,
+      phoneNumber: newNumber,
+      address: newAddress,
+    };
+    setPersonalInfo(newPersonalInfo);
+    setNewName('');
+    setNewEmail('');
+    setNewNumber('');
+    setNewAddress('');
   };
 
   const handleName = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleEmail = (event) => {
+    setNewEmail(event.target.value);
+  };
+
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const handleAddress = (event) => {
+    setNewAddress(event.target.value);
+  };
+
   return (
     <main>
       <Header />
       <div className="contentContainer">
-        <General />
-        <Education />
-        <Experience />
-        <button type="submit">Submit</button>
+        <General
+          nameValue={newName}
+          emailValue={newEmail}
+          numberValue={newNumber}
+          addressValue={newAddress}
+          onSubmit={addPersonalInfo}
+          onChangeName={handleName}
+          onChangeEmail={handleEmail}
+          onChangeNumber={handleNumber}
+          onChangeAddress={handleAddress}
+        />
+        {/* <Education />
+        <Experience /> */}
+        <button form="submit-form" type="submit">
+          Submit
+        </button>
+        {personalInfo.fullName}
+        {personalInfo.email}
+        {personalInfo.phoneNumber}
+        {personalInfo.address}
       </div>
     </main>
   );
--- src/components/General.js
+++ src/components/General.js
@@ -1,12 +1,40 @@
-const General = () => {
+const General = ({
+  nameValue,
+  emailValue,
+  numberValue,
+  addressValue,
+  onSubmit,
+  onChangeName,
+  onChangeEmail,
+  onChangeNumber,
+  onChangeAddress,
+}) => {
   return (
     <section className="general">
       <h2>General Info</h2>
-      <form>
-        <input placeholder="Full Name" />
-        <input placeholder="Email" />
-        <input placeholder="Phone Number" type="tel" />
-        <input placeholder="Address" />
+      <form id="submit-form" onSubmit={onSubmit}>
+        <input
+          value={nameValue}
+          onChange={onChangeName}
+          placeholder="Full Name"
+        />
+        <input
+          value={emailValue}
+          onChange={onChangeEmail}
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          value={numberValue}
+          onChange={onChangeNumber}
+          placeholder="Phone Number"
+          type="tel"
+        />
+        <input
+          value={addressValue}
+          onChange={onChangeAddress}
+          placeholder="Address"
+        />
       </form>
     </section>
   );
